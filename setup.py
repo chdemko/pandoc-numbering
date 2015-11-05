c6903e0eaea45ddcc099a2302b395faac17f31d8
Christophe Demko <chdemko@gmail.com>
2015-11-05 10:30:35 +0100

    Removing licence classifier

--- setup.py
+++ setup.py
@@ -60,9 +60,6 @@ setup(
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
 
-        # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: CEA CNRS Inria Logiciel Libre License, version 2.1 (CeCILL-2.1)',
-
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 2',
