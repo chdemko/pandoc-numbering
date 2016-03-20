29f3e4864dea965e05e4c8d55eec704fa855d8be
Christophe Demko <chdemko@gmail.com>
2016-03-20 16:24:02 +0100

    Removing u

--- tests/test_toIdentifier.py
+++ tests/test_toIdentifier.py
@@ -4,6 +4,6 @@ from unittest import TestCase
 from pandoc_numbering import toIdentifier
 
 def test_toIdentifier():
-    assert toIdentifier(u'0123   Ê   à') == 'e-a'
+    assert toIdentifier('0123   Ê   à') == 'e-a'
 
 
