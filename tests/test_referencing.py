68e2db8abb8c87725e2b4e410c600b20035d5a81
Christophe Demko <chdemko@gmail.com>
2016-03-23 16:27:37 +0100

    Adding tests

--- tests/test_referencing.py
+++ tests/test_referencing.py
@@ -104,3 +104,76 @@ def test_referencing_unexisting():
         pandoc_numbering.referencing('Link', link2, '', {})
         assert link2[1] == []
 
+def test_referencing_headers():
+    pandoc_numbering.count = {}
+    pandoc_numbering.information = {}
+    pandoc_numbering.headers = [0, 0, 0, 0, 0, 0]
+    pandoc_numbering.numbering(
+        'Header',
+        [1, ['chapter', [], []], [{'c': 'Chapter', 't': 'Str'}]],
+        '',
+        {}
+    )
+    pandoc_numbering.numbering(
+        'Para',
+        [{'c': u'Theorem', 't': 'Str'}, {'c': [], 't': 'Space'}, {'c': '#.#theorem:first', 't': 'Str'}],
+        '',
+        {}
+    )
+    if pandoc_numbering.pandoc_version() < '1.16':
+        link1 = [
+            [],
+            ['#theorem:first', '']
+        ]
+        pandoc_numbering.referencing('Link', link1, '', {})
+        assert link1[0] == [{'t': 'Str', 'c': 'Theorem'}, {'t': 'Space', 'c': []},{'t': 'Str', 'c': '1.1'}]
+
+        link2 = [
+            [
+                {'c': 'See', 't': 'Str'},
+                {'c': [], 't': 'Space'},
+                {'c': 'theorem', 't': 'Str'},
+                {'c': [], 't': 'Space'},
+                {'c': '#', 't': 'Str'}
+            ],
+            ['#theorem:first', 'theorem #']
+        ]
+        pandoc_numbering.referencing('Link', link2, '', {})
+        assert link2[0] == [
+                {'c': 'See', 't': 'Str'},
+                {'c': [], 't': 'Space'},
+                {'c': 'theorem', 't': 'Str'},
+                {'c': [], 't': 'Space'},
+                {'c': '1.1', 't': 'Str'}
+            ]
+        assert link2[1][1] == 'theorem 1.1'
+    else:
+        link1 = [
+            ['', [], []],
+            [],
+            ['#theorem:first', '']
+        ]
+        pandoc_numbering.referencing('Link', link1, '', {})
+        assert link1[1] == [{'t': 'Str', 'c': 'Theorem'}, {'t': 'Space', 'c': []},{'t': 'Str', 'c': '1.1'}]
+
+        link2 = [
+            ['', [], []],
+            [
+                {'c': 'See', 't': 'Str'},
+                {'c': [], 't': 'Space'},
+                {'c': 'theorem', 't': 'Str'},
+                {'c': [], 't': 'Space'},
+                {'c': '#', 't': 'Str'}
+            ],
+            ['#theorem:first', 'theorem #']
+        ]
+        pandoc_numbering.referencing('Link', link2, '', {})
+        assert link2[1] == [
+                {'c': 'See', 't': 'Str'},
+                {'c': [], 't': 'Space'},
+                {'c': 'theorem', 't': 'Str'},
+                {'c': [], 't': 'Space'},
+                {'c': '1.1', 't': 'Str'}
+            ]
+        assert link2[2][1] == 'theorem 1.1'
+
