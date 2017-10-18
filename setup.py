817353e9b3fd21654f82f55ea6e58b62b94be58b
Christophe Demko <chdemko@gmail.com>
2017-10-18 13:28:16 +0200

    Adding pypandoc

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='2.1.0',
+    version='2.1.1',
 
     # The project's description
     description='A pandoc filter for automatic numbering',
@@ -103,7 +103,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['pandocfilters>=1.4'],
+    install_requires=['pandocfilters>=1.4', 'pypandoc'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
