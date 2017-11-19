178a5059ca0e97bef5b46710ffaeb5e223393e06
Christophe Demko <chdemko@gmail.com>
2017-11-19 14:42:42 +0100

    Change version and update dependencies

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='2.2.1',
+    version='2.3.0',
 
     # The project's description
     description='A pandoc filter for automatic numbering',
@@ -102,7 +102,10 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['pandocfilters>=1.4', 'pypandoc'],
+    install_requires=[
+        'pandocfilters>=1.4',
+        'pypandoc>=1.4'
+    ],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
