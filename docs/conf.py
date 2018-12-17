18625613f2e35214cd82e6f2fe1c35d6a6225231
Christophe Demko <chdemko@gmail.com>
2018-12-17 13:05:03 +0100

    Prepare 3.1.0.1

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,7 @@ author = 'Christophe Demko'
 # The short X.Y version
 version = '3.1'
 # The full version, including alpha/beta/rc tags
-release = '3.1.0'
+release = '3.1.0.1'
 
 
 # -- General configuration ---------------------------------------------------
