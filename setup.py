2c96a722d3d447d814e91d8821e13494be98c028
Christophe Demko <chdemko@gmail.com>
2020-03-30 20:38:44 +0200

    Fixes #14

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version="3.2.0.0",
+    version="3.2.1.0",
     # The project's description
     description="A pandoc filter for automatic numbering",
     long_description=long_description,
@@ -80,7 +80,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=["panflute>=1.12", "pypandoc>=1.4"],
+    install_requires=["panflute<1.12", "pypandoc>=1.4"],
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
     # for example:
